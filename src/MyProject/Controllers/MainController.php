99ad6e79af7a436c57de1d7593f6bba1551c586b
mediaphag <andrew.golovko@gmail.com>
2022-01-12 17:37:39 +0200

    remove Subtractor.php Summator.php TestCron.php and do some refactoring in MainController

--- src/MyProject/Controllers/MainController.php
+++ src/MyProject/Controllers/MainController.php
@@ -13,12 +13,13 @@ class MainController extends AbstractController
 
     public function page(int $pageNum)
     {
+        $pagesCount = Article::getPagesCount(5);
         $this->view->renderHtml('main/main.php', [
             'articles' => Article::getPage($pageNum, 5),
-            'pagesCount' => Article::getPagesCount(5),
+            'pagesCount' => $pagesCount,
             'currentPageNum' => $pageNum,
             'previousPageLink' => $pageNum > 1 ? '/' . ($pageNum - 1) : null,
-            'nextPageLink' => $pageNum < Article::getPagesCount(5) ? '/' . ($pageNum + 1) : null
+            'nextPageLink' => $pageNum < $pagesCount ? '/' . ($pageNum + 1) : null
         ]);
     }
 }
